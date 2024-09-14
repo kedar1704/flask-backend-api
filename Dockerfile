631f0bf44865685e6363628a14b764125fee4c6e
root <root@ip-172-31-0-60.ec2.internal>
2024-09-14 15:00:26 +0000

    Making changes to app

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,7 @@
 FROM python:3.9-alpine
 MAINTAINER kedar1704
 WORKDIR /app
+RUN mkdir -p /var/logs
 COPY requirements.txt .
 COPY app.py .
 RUN pip install -r requirements.txt
--- app.py
+++ app.py
@@ -11,7 +11,7 @@ logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     handlers=[
                         logging.StreamHandler(),  
-                        logging.FileHandler('app.log')  
+                        logging.FileHandler('/var/logs/app.log')  
                     ])
 
 
